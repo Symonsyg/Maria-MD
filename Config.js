bac593af24714c5b6ebca83fe15e2c91d2b6e3b7
Symonsyg <165176521+Symonsyg@users.noreply.github.com>
2024-03-27 16:22:35 +0200

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,11 +2,11 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['260777086572']
+global.ownername = "😎SY G MB"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.location = "zambian"
 
 global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
 
